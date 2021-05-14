6eb1f3cd7d401c27ff019ed1ef8aa24daea6aa36
Colin-K88 <colinkee@hotmail.com>
2021-05-14 14:44:16 +0000

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+let currentQuestion = 0
+let score = 0
+let totalQuestion = questions.length
+
+const container = document.getElementById('quizContainer');
+const questionEl = document.getElementById('question');
+const opt1 = document.getElementById('opt1');
+const opt2 = document.getElementById('opt2');
+const opt3 = document.getElementById('opt3');
+const opt4 = document.getElementById('opt4');
+const nextButton = document.getElementById('submitButton');
+const resultCont = document.getElementById('result');
