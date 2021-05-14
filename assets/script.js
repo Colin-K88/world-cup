3d369762cf0f221c21c90f41ebbf61294f44736b
Colin-K88 <colinkee@hotmail.com>
2021-05-14 14:46:19 +0000

    loadQuestion function added

--- assets/script.js
+++ assets/script.js
@@ -10,3 +10,18 @@ const opt3 = document.getElementById('opt3');
 const opt4 = document.getElementById('opt4');
 const nextButton = document.getElementById('submitButton');
 const resultCont = document.getElementById('result');
+
+function loadQuestion (questionIndex) {
+	let q = questions[questionIndex];
+	questionEl.textContent = (questionIndex + 1) + '. ' + q.question;
+	opt1.textContent = q.option1;
+	opt2.textContent = q.option2;
+	opt3.textContent = q.option3;
+	opt4.textContent = q.option4;
+};
+function loadNextQuestion () {
+	let selectedOption = document.querySelector('input[type=radio]:checked');
+	if(!selectedOption){
+		alert('Please select your answer!');
+		return;
+	}
